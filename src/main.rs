32813e0cb4fa487e657f9c51c21d6da1e6856e3f
Pallos Gábor György <pallosgabor99@gmail.com>
2024-01-05 14:39:43 +0100

    pass stage 3

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,49 @@ use std::{env, str::FromStr};
 // Available if you need it!
 // use serde_bencode
 
+fn decode_bencoded_string(encoded_value: &str) -> (serde_json::Value, &str) {
+    let colon_index = encoded_value.find(':').unwrap();
+    let number_string = &encoded_value[..colon_index];
+    let number = number_string.parse::<i64>().unwrap();
+    let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+    return (serde_json::Value::String(string.to_string()), &encoded_value[colon_index + 1 + number as usize..]);
+}
+fn decode_bencoded_integer(encoded_value: &str) -> (serde_json::Value, &str) {
+    let end_index = encoded_value.find('e').unwrap();
+    let number_string = &encoded_value[1..end_index];
+    let number = serde_json::Number::from_str(number_string).unwrap();
+    return (serde_json::Value::Number(number), &encoded_value[end_index+1..]);
+}
+fn decode_bencoded_list(encoded_value: &str) -> (serde_json::Value, &str) {
+    let mut list:Vec<serde_json::Value> = Vec::new();
+    let mut encoded_value = encoded_value;
+    loop {
+        let first_char = match encoded_value.chars().next() {
+            Some(c) => c,
+            None => break,
+        };
+        let (value, remaining_string) = match first_char {
+            c  if c.is_digit(10) => decode_bencoded_string(encoded_value),
+            'i' => decode_bencoded_integer(encoded_value),
+            'l' => decode_bencoded_list(&encoded_value[1..]),
+            'e' => {encoded_value = &encoded_value[1..]; break},
+            _ => panic!("Unhandled encoded value: {}", encoded_value)
+        };
+        list.push(value);
+        encoded_value = remaining_string;
+
+    }
+    return (serde_json::Value::Array(list), encoded_value);
+}
+
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else if encoded_value.chars().next().unwrap() == 'i' {
-        let end_index = encoded_value.find('e').unwrap();
-        let number_string = &encoded_value[1..end_index];
-        let number = serde_json::Number::from_str(number_string).unwrap();
-        return serde_json::Value::Number(number);
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
-    }
+    return match encoded_value.chars().next().unwrap() {
+        c  if c.is_digit(10) => decode_bencoded_string(encoded_value).0,
+        'i' => decode_bencoded_integer(encoded_value).0,
+        'l' => decode_bencoded_list(&encoded_value[1..]).0,
+        _ => panic!("Unhandled encoded value: {}", encoded_value)
+    };
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
